64a2214611b93a6251b14e02b31748fde7045da0
shrutib2002 <shrutib9393@gmail.com>
2024-12-25 21:13:03 +0530

    Collection, Cart,Placeorder,Orders completed

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewLetterBox from '../components/NewLetterBox'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <LatestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewLetterBox/>
+    </div>
+  )
+}
+
+export default Home
